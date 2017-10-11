720adf9d090e5fb497deef5a48247323df48e568
Ahchuang <E0036072@u.nus.edu>
2017-10-11 19:48:46 +0800

    Update Style

--- src/main/java/seedu/address/logic/commands/BirthdayCommand.java
+++ src/main/java/seedu/address/logic/commands/BirthdayCommand.java
@@ -1,10 +1,10 @@
 package seedu.address.logic.commands;
 
-import java.util.List;
-import java.util.Set;
-
 import static seedu.address.logic.parser.CliSyntax.PREFIX_BIRTHDAY;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.List;
+import java.util.Set;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
